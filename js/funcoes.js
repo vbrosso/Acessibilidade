e5fc9babcd895c2f4be4dab1f03e9d2190a52851
vbrosso <vbrosso@gmail.com>
2015-02-09 08:50:51 -0200

    Update funcoes.js

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,171 +1,127 @@
 
 // JavaScript Document
 $( document ).ready(function() {
-	//ACESSIBILIDADE
-	/*$('p, h1, h2, h3, h4, h5, legend, span, strong, a, label, p a, div, td').addClass('fontSize');*/
-		
 	// Cria os Cookies
-    if ($.cookie('contraste1') === "true") {
-		$('body').addClass('contraste1');
-    }
+		    if ($.cookie('contraste1') === "true") {
+				$('body').addClass('contraste1');
+		    }
 
-    if ($.cookie('contraste2') === "true") {
-		$('body').addClass('contraste2');
-    }
-	
-    if ($.cookie('resetCookie') === "true") {
-    	$('body').removeClass('contraste1');
-		$('body').removeClass('contraste2');
-    }
-	
-	if ($.cookie('librasCookie') === "ativo") {
-		$('a.btn-libras').attr('id','ativo');
-		$('.contEsq .divLibras').addClass('abrevideo');
-		$('a.btn-libras').addClass('ativo');
-		
-    }else{
-		$('.contEsq .divLibras').removeClass('abrevideo');
-		$('a.btn-libras').removeClass('ativo');
-		$('a.btn-libras').attr('id','');
-    }
-	
-	if ($.cookie('tamanhoLetra') != null) {
-		var iTamanho = $.cookie('tamanhoLetra');
-		console.log(iTamanho);
-		$(".fontSize, .fontSize15").each(function(){
-			var tamanho = $(this).css('font-size').substring(0, 2);
-			$(this).attr("style", "font-size:"+(parseInt(tamanho)+parseInt(iTamanho))+"px;");
-		});
-    } else {
-    	var iTamanho = 0;
-    }
-	
-    // Acoes nos botoes de acessibilidade
-    $(".btn-contraste1").click(function () {
-		$.removeCookie('contraste2', { path: '/' });
-		$.removeCookie('resetCookie', { path: '/' });
-		if (!($.cookie('contraste1') === "true")) {
-            $('body').removeClass('contraste2');
-			$('body').addClass('contraste1');
-            $.cookie('contraste1','true',{path:'/'});
-        }       
-        else {
-            $.cookie('contraste1','false',{path:'/'});
-        }
-		return false;
-    });
-	
-    $(".btn-contraste2").click(function () {
-		$.removeCookie('contraste1', { path: '/' });
-		$.removeCookie('resetCookie', { path: '/' });
-        if (!($.cookie('contraste2') === "true")) {
-            $('body').removeClass('contraste1');
-			$('body').addClass('contraste2');
-            $.cookie('contraste2','true',{path:'/'});
-        }       
-        else {
-            $.cookie('contraste2','false',{path:'/'});
-        }
-		return false;		
-    });
-	
-	 $(".fontResizer_reset").click(function () {
-		 $.removeCookie('contraste2', { path: '/' });
-		 $.removeCookie('contraste1', { path: '/' });
-
-		// $(".fontSize, .fontSize15").each(function(){
-		// 	var tamanho = $(this).css('font-size').substring(0, 2);
-		// 	$(this).attr("style", "font-size:"+tamanho+"px;");
-		// 	$.cookie('tamanhoLetra',iTamanho,{ path: '/' });
-		// });
-	 	iTamanho = 0;
-        if (!($.cookie('resetCookie') === "true")) {
-            $('body').removeClass('contraste1');
-			$('body').removeClass('contraste2');
-            $.cookie('resetCookie','true',{path:'/'});
-
-        	$('.fontSize').removeAttr('style');
-        	$('.fontSize15').removeAttr('style');
-            $.removeCookie('tamanhoLetra', { path: '/' });
-        }       
-        else {
-        	$('.fontSize').removeAttr('style');
-        	$('.fontSize15').removeAttr('style');
-            $.removeCookie('tamanhoLetra', { path: '/' });
-            $.cookie('resetCookie','false',{path:'/'});
-        }
-		return false;		
-    });
-	
-	$(".btn-libras").click(function () {
-		
-		if($(this).attr('id')=='ativo'){
-			$('.contEsq .divLibras').removeClass('abrevideo');
-			$('a.btn-libras').removeClass('ativo');
-			$('a.btn-libras').attr('id','');
+		    if ($.cookie('contraste2') === "true") {
+				$('body').addClass('contraste2');
+		    }
 			
-		}else{
-			$('a.btn-libras').attr('id','ativo');
-			$('.contEsq .divLibras').addClass('abrevideo');
-			$('a.btn-libras').addClass('ativo');
-		}
-		$.cookie('librasCookie',$(this).attr('id'),{ path: '/' });
+		    if ($.cookie('resetCookie') === "true") {
+		    	$('body').removeClass('contraste1');
+				$('body').removeClass('contraste2');
+		    }
+			
+			if ($.cookie('librasCookie') === "ativo") {
+				$('a.btn-libras').attr('id','ativo');
+				$('.contEsq .divLibras').addClass('abrevideo');
+				$('a.btn-libras').addClass('ativo');
+				
+		    }else{
+				$('.contEsq .divLibras').removeClass('abrevideo');
+				$('a.btn-libras').removeClass('ativo');
+				$('a.btn-libras').attr('id','');
+		    }
 
-    });
-	
-	
-	$(function(){
+			$(".fontResizer_add").click(function () {
+				maisFont();
+			});
+			$(".fontResizer_minus").click(function () {
+				menosFont();
+			});
+			$(".fontResizer_reset").click(function () {
+				resetFont();
+			});
+			
+			(function(){
+				var tam = $.cookie('tamanhoLetra');
+				$('body').css("font-size",tam+"px");
+				console.log("TAMANHO INICIAL",$.cookie('tamanhoLetra'));
+			})();
 
-		tamanhoMax = 28;
-		tamanhoMin = 10;
+			function maisFont(){
+				var tamanho = $('body').css("font-size");
+				var maisUm = parseInt(tamanho.substr(0, 2));
+				maisUm++;
+				$.cookie('tamanhoLetra', maisUm,{ path: '/' });
+				$('body').css("font-size",maisUm+"px");
+				console.log("MAiS",$.cookie('tamanhoLetra'));
+			}
+			function menosFont(){
+				var tamanho = $('body').css("font-size");
+				var maisUm = parseInt(tamanho.substr(0, 2));
+				maisUm--;
+				$.cookie('tamanhoLetra', maisUm,{ path: '/' });
+				$('body').css("font-size",maisUm+"px");
+			}
+			function resetFont(){
+				$('body').css("font-size","12px");
+
+				$.removeCookie('contraste2', { path: '/' });
+		 		$.removeCookie('contraste1', { path: '/' });
+
+		 		if (!($.cookie('resetCookie') === "true")) {
+		            $('body').removeClass('contraste1');
+					$('body').removeClass('contraste2');
+		            $.cookie('resetCookie','true',{path:'/'});
+
+		        	$('.fontSize').removeAttr('style');
+		        	$('.fontSize15').removeAttr('style');
+		            $.removeCookie('tamanhoLetra', { path: '/' });
+		        }       
+		        else {
+		        	$('.fontSize').removeAttr('style');
+		        	$('.fontSize15').removeAttr('style');
+		            $.removeCookie('tamanhoLetra', { path: '/' });
+		            $.cookie('resetCookie','false',{path:'/'});
+		        }
+			}
+
+			// Acoes nos botoes de acessibilidade
+		    $(".btn-contraste1").click(function () {
+				$.removeCookie('contraste2', { path: '/' });
+				$.removeCookie('resetCookie', { path: '/' });
+				if (!($.cookie('contraste1') === "true")) {
+		            $('body').removeClass('contraste2');
+					$('body').addClass('contraste1');
+		            $.cookie('contraste1','true',{path:'/'});
+		        }       
+		        else {
+		            $.cookie('contraste1','false',{path:'/'});
+		        }
+				return false;
+		    });
+			
+		    $(".btn-contraste2").click(function () {
+				$.removeCookie('contraste1', { path: '/' });
+				$.removeCookie('resetCookie', { path: '/' });
+		        if (!($.cookie('contraste2') === "true")) {
+		            $('body').removeClass('contraste1');
+					$('body').addClass('contraste2');
+		            $.cookie('contraste2','true',{path:'/'});
+		        }       
+		        else {
+		            $.cookie('contraste2','false',{path:'/'});
+		        }
+				return false;		
+		    });
+		    $(".btn-libras").click(function () {
 		
-		$(".btn-aumenta").click(function(){
-			iTamanho++;
-			console.log(iTamanho);
-			$(".fontSize, .fontSize15").each(function(){
-				tamanho = $(this).css('font-size').substring(0, 2);
-				if (tamanho <= tamanhoMax){
-					tamanho = tamanho-iTamanho+1;
-					$(this).attr("style", "font-size:"+(parseInt(tamanho)+parseInt(iTamanho))+"px;");
-					$.cookie('tamanhoLetra', iTamanho,{ path: '/' });
+				if($(this).attr('id')=='ativo'){
+					$('.contEsq .divLibras').removeClass('abrevideo');
+					$('a.btn-libras').removeClass('ativo');
+					$('a.btn-libras').attr('id','');
+					
+				}else{
+					$('a.btn-libras').attr('id','ativo');
+					$('.contEsq .divLibras').addClass('abrevideo');
+					$('a.btn-libras').addClass('ativo');
 				}
-			});
-			// if(tamanhoLetra <= tamanhoMax){
-			// 	tamanhoLetra++;
-			// 	$('.fontSize').css({'fontSize': tamanhoLetra});
-			// 	$.cookie('tamanhoLetra',tamanhoLetra,{ path: '/' });
-			// }
-			// if(tamanhoLetra15 <= tamanhoMax15){
-			// 	tamanhoLetra15++;
-			// 	$('.fontSize15').css({'fontSize': tamanhoLetra15});
-			// 	$.cookie('tamanhoLetra15',tamanhoLetra15,{ path: '/' });
-			// }
-			return false;			
-		});
-	 
-		$(".btn-diminui").click(function(){
-			iTamanho--;
-			console.log(iTamanho);
-			$(".fontSize, .fontSize15").each(function(){
-				tamanho = $(this).css('font-size').substring(0, 2);
-				if (tamanho >= tamanhoMin){
-					tamanho = tamanho-iTamanho-1;
-					$(this).attr("style", "font-size:"+(parseInt(tamanho)+parseInt(iTamanho))+"px;");
-					$.cookie('tamanhoLetra', iTamanho,{ path: '/' });
-				}
-			});
-			// if(tamanhoLetra >= tamanhoMin){
-			// 	tamanhoLetra--;
-			// 	$('.fontSize').css({'fontSize': tamanhoLetra});
-			// 	$.cookie('tamanhoLetra',tamanhoLetra,{ path: '/' });
-			// }
-			// if(tamanhoLetra15 >= tamanhoMin15){
-			// 	tamanhoLetra15--;
-			// 	$('.fontSize15').css({'fontSize': tamanhoLetra15});
-			// 	$.cookie('tamanhoLetra15',tamanhoLetra15,{ path: '/' });
-			// }
-			return false;		
-		});
+				$.cookie('librasCookie',$(this).attr('id'),{ path: '/' });
 
-	})
+		    });
+
 });
